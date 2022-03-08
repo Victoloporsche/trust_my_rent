c4eafb1eb66f5e8d094c2422f461bc44561e3d15
Victor Uwaje <v.uwaje@enviosystems.com>
2022-03-08 14:44:26 +0100

    log mlflow model

---
+++ src/log_production_model.py
@@ -0,0 +1,70 @@
+import argparse
+import mlflow
+from mlflow.tracking import MlflowClient
+from pprint import pprint
+import joblib
+import yaml
+import pandas as pd
+
+
+def read_params(config_path):
+    with open(config_path) as yaml_file:
+        config = yaml.safe_load(yaml_file)
+    return config
+
+
+def get_data(config_path) -> pd.DataFrame:
+    config = read_params(config_path)
+    data_path = config['data_source']['sql_source']
+    df = pd.read_csv(data_path, sep=',', encoding='utf-8')
+    return df
+
+
+def log_production_model(config_path):
+    config = read_params(config_path)
+
+    mlflow_config = config["mlflow_config"]
+
+    model_name = mlflow_config["registered_model_name"]
+
+    remote_server_uri = mlflow_config["remote_server_uri"]
+
+    mlflow.set_tracking_uri(remote_server_uri)
+
+    runs = mlflow.search_runs(experiment_ids=["1"])
+    lowest = runs["metrics.mae"].sort_values(ascending=True)[0]
+    lowest_run_id = runs[runs["metrics.mae"] == lowest]["run_id"][0]
+
+    client = MlflowClient()
+    for mv in client.search_model_versions(f"name='{model_name}'"):
+        mv = dict(mv)
+
+        if mv["run_id"] == lowest_run_id:
+            current_version = mv["version"]
+            logged_model = mv["source"]
+            pprint(mv, indent=4)
+            client.transition_model_version_stage(
+                name=model_name,
+                version=current_version,
+                stage="Production"
+            )
+        else:
+            current_version = mv["version"]
+            client.transition_model_version_stage(
+                name=model_name,
+                version=current_version,
+                stage="Staging"
+            )
+
+    loaded_model = mlflow.pyfunc.load_model(logged_model)
+
+    model_path = config["webapp_model_dir"]
+
+    joblib.dump(loaded_model, model_path)
+
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser()
+    args.add_argument('--config', default='params.yaml')
+    parsed_args = args.parse_args()
+    log_production_model(config_path=parsed_args.config)
